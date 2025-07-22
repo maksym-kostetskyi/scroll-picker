854840fc587b14ef4c3b958fa0b52309c477dc17
Maksym <mkost996@gmail.com>
2025-07-22 21:10:24 +0300

    fix-routing-gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/scroll-picker/",
+  base: process.env.NODE_ENV === "production" ? "/scroll-picker/" : "/",
 });
